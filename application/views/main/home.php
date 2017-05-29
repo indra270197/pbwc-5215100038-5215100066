9f9dbbdcc2d3c31a918667c5a05e46769a8ff1c9
ekaputri97 <ekaputriharyanti@gmail.com>
2017-05-30 01:25:03 +0700

    Update home.php

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -7,37 +7,41 @@
                 <li data-target="#main-slider" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
-                <div class="item active" style="background-image: url(<?php echo base_url(); ?>assets/img/slides/1.jpg)">
+                <div class="item active" style="background-image: url(img/slides/1.jpg)">
                     <div class="container">
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="carousel-content centered">
-                                    <h2 align="center" class="animation animated-item-1">East Java Geophysics</h2>
-                                    <p  align="center" class="animation animated-item-2">"Seeing the Invisible"</p>
+                                    <h2 class="animation animated-item-1">East Java Geophysics</h2>
+                                    <p class="animation animated-item-2">Sed mattis enim in nulla blandit tincidunt. Phasellus vel sem convallis, mattis est id, interdum augue. Integer luctus massa in arcu euismod venenatis. </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div><!--/.item-->
-                <div class="item" style="background-image: url(<?php echo base_url(); ?>assets/img/slides/2.jpg)">
+                <div class="item" style="background-image: url(img/slides/2.jpg)">
                     <div class="container">
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="carousel-content center centered">
-                                    <h2 align="center" class="animation animated-item-1">East Java Geophysics</h2>
-                                    <p  align="center" class="animation animated-item-2">"Seeing the Invisible"</p>
+                                    <h2 class="animation animated-item-1">Powerful and Responsive HTML Template</h2>
+                                    <p class="animation animated-item-2">Phasellus adipiscing felis a dictum dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at ligula risus. </p>
+                                    <br>
+                                    <a class="btn btn-md animation animated-item-3" href="#">Learn More</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div><!--/.item-->
-                <div class="item" style="background-image: url(<?php echo base_url(); ?>assets/img/slides/3.jpg)">
+                <div class="item" style="background-image: url(img/slides/3.jpg)">
                     <div class="container">
                         <div class="row">
                             <div class="col-sm-12">
                                 <div class="carousel-content centered">
-                                    <h2 align="center" class="animation animated-item-1">East Java Geophysics</h2>
-                                    <p align="center" class="animation animated-item-2">"Seeing the Invisible"</p>
+                                    <h2 class="animation animated-item-1">Works Seamlessly Well on All Devices</h2>
+                                    <p class="animation animated-item-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae euismod lacus. Maecenas in tempor lectus. Nam mattis, odio ut dapibus ornare, libero. </p>
+                                    <br>
+									<a class="btn btn-md animation animated-item-3" href="#">Learn More</a>
                                 </div>
                             </div>
                         </div>
@@ -54,6 +58,21 @@
     </section><!--/#main-slider-->
 
 
+		<!-- Call to Action Bar -->
+	    <div class="section section-dark">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="calltoaction-wrapper">
+							<h3>Welcome to <span style="color:#aec62c; text-transform:uppercase;font-size:24px;">Basica!</span> A free fully responsive Bootstrap 3 HTML5 template!</h3> <a href="http://www.vactualart.com/portfolio-item/basica-a-free-html5-template/" class="btn btn-orange">Download here!</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- End Call to Action Bar -->
+
+
 		<!-- Services -->
         <div class="section section-white">
 	        <div class="container">
@@ -61,25 +80,25 @@
 	        		<div class="col-md-4 col-sm-6">
 	        			<div class="service-wrapper">
 		        			<i class="icon-home"></i>
-		        			<h3>About us</h3>
-		        			<p>Lebih tahu lanjut tentang kami</p>
-		        			<a href="<?php echo base_url('main/about'); ?>" class="btn">Read more</a>
+		        			<h3>Aliquam in adipiscing</h3>
+		        			<p>Praesent rhoncus mauris ac sollicitudin vehicula. Nam fringilla turpis turpis, at posuere turpis aliquet sit amet condimentum</p>
+		        			<a href="#" class="btn">Read more</a>
 		        		</div>
 	        		</div>
 	        		<div class="col-md-4 col-sm-6">
 	        			<div class="service-wrapper">
 		        			<i class="icon-briefcase"></i>
-		        			<h3>Cek Pesanan</h3>
-		        			<p>Mengecek status pesanan</p>
-		        			<a href="<?php echo base_url('usernotif_pesan'); ?>" class="btn">Cek</a>
+		        			<h3>Curabitur mollis</h3>
+		        			<p>Suspendisse eget libero mi. Fusce ligula orci, vulputate nec elit ultrices, ornare faucibus orci. Aenean lectus sapien, vehicula</p>
+		        			<a href="#" class="btn">Read more</a>
 		        		</div>
 	        		</div>
 	        		<div class="col-md-4 col-sm-6">
 	        			<div class="service-wrapper">
 		        			<i class="icon-calendar"></i>
-		        			<h3>Pesan Sekarang</h3>
-		        			<p>Melayani dengan kualitas terbaik dan sepenuh hati</p>
-		        			<a href="<?php echo base_url('pesan'); ?>" class="btn">Pesan</a>
+		        			<h3>Vivamus mattis</h3>
+		        			<p>Phasellus posuere et nisl ac commodo. Nulla facilisi. Sed tincidunt bibendum cursus. Aenean vulputate aliquam risus rutrum scelerisque</p>
+		        			<a href="#" class="btn">Read more</a>
 		        		</div>
 	        		</div>
 	        	</div>
@@ -108,7 +127,7 @@
 						<figcaption>
 							<h3>Settings</h3>
 							<span>Jacob Cummings</span>
-							<a href="#">Take a look</a>
+							<a href="<?php echo base_url(); ?>assets/portfolio-item.html">Take a look</a>
 						</figcaption>
 					</figure>
 	        	</div>
@@ -118,7 +137,7 @@
 						<figcaption>
 							<h3>Camera</h3>
 							<span>Jacob Cummings</span>
-							<a href="#">Take a look</a>
+							<a href="<?php echo base_url(); ?>assets/portfolio-item.html">Take a look</a>
 						</figcaption>
 					</figure>
 				</div>
@@ -128,7 +147,7 @@
 						<figcaption>
 							<h3>Music</h3>
 							<span>Jacob Cummings</span>
-							<a href="#">Take a look</a>
+							<a href="<?php echo base_url(); ?>assets/portfolio-item.html">Take a look</a>
 						</figcaption>
 					</figure>
 				</div>
@@ -138,7 +157,7 @@
 						<figcaption>
 							<h3>Safari</h3>
 							<span>Jacob Cummings</span>
-							<a href="#">Take a look</a>
+							<a href="<?php echo base_url(); ?>assets/portfolio-item.html">Take a look</a>
 						</figcaption>
 					</figure>
 				</div>
@@ -148,7 +167,7 @@
 						<figcaption>
 							<h3>Phone</h3>
 							<span>Jacob Cummings</span>
-							<a href="#">Take a look</a>
+							<a href="<?php echo base_url(); ?>assets/portfolio-item.html">Take a look</a>
 						</figcaption>
 					</figure>
 				</div>
@@ -158,7 +177,7 @@
 						<figcaption>
 							<h3>Game Center</h3>
 							<span>Jacob Cummings</span>
-							<a href="#">Take a look</a>
+							<a href="<?php echo base_url(); ?>assets/portfolio-item.html">Take a look</a>
 						</figcaption>
 					</figure>
 				</div>
@@ -169,3 +188,29 @@
 		<!-- Our Portfolio -->
 
 <hr>
+
+		<!-- Our Clients -->
+	    <div class="section">
+	    	<div class="container">
+
+				<div class="section-title">
+				<h1>Our Success Stories</h1>
+				</div>
+
+				<div class="clients-logo-wrapper text-center row">
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-1.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-2.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-3.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-4.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-5.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-6.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-7.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-8.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-9.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-10.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-11.jpg" alt="Client Name"></a></div>
+					<div class="col-lg-1 col-md-1 col-sm-3 col-xs-6"><a href="#"><img src="<?php echo base_url(); ?>assets/img/logos/logo-12.jpg" alt="Client Name"></a></div>
+				</div>
+			</div>
+	    </div>
+	    <!-- End Our Clients -->
