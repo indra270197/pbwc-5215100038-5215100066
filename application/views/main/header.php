bc7d3e49b47a16245b48a6a529b95f01c9fe00bb
ekaputri97 <ekaputriharyanti@gmail.com>
2017-05-30 01:23:31 +0700

    Update header.php

--- application/views/main/header.php
+++ application/views/main/header.php
@@ -16,20 +16,20 @@
     <title>East Java Geophysics</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom CSS -->
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
-    <link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
+    <link href="css/custom.css" rel="stylesheet">
 
 	<script src="//use.edgefonts.net/bebas-neue.js"></script>
 
     <!-- Custom Fonts & Icons -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600,800' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/icomoon-social.css">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/icomoon-social.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
 
-	<script src="<?php echo base_url(); ?>assets/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<script src="js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
 
 </head>
@@ -49,17 +49,29 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="<?php echo base_url(); ?>assets/index.html"><img src="<?php echo base_url(); ?>assets/img/logo.png" alt="Basica"></a>
+                <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="Basica"></a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="<?php echo base_url('main'); ?>">Home</a></li>
-                    <li><a href="<?php echo base_url('main/about'); ?>">About Us</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Hai <?php echo $this->session->userdata("nama"); ?></a></li>
-                    <li><a href="<?php echo base_url('user_login/logout'); ?>">Log out</a></li>
+                    <li class="active"><a href="index.html">Home</a></li>
+                    <li><a href="about-us.html">About Us</a></li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="portfolio.html">Portfolio</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Dropdown Menu 1</a></li>
+                            <li><a href="#">Dropdown Menu 2</a></li>
+                            <li><a href="#">Dropdown Menu 3</a></li>
+                            <li><a href="#">Dropdown Menu 4</a></li>
+                            <li><a href="#">Dropdown Menu 5</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Terms of Use</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact-us.html">Contact</a></li>
                 </ul>
             </div>
         </div>
